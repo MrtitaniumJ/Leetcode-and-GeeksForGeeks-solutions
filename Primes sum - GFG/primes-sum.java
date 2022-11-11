c1b910099ec94fc5cd5a2c55b9350e7fc8ea29c2
Jatin Sharma <87287391+MrtitaniumJ@users.noreply.github.com>
2022-11-11 22:51:09 +0530

    Added solution - LeetHub

--- Primes sum - GFG/primes-sum.java
+++ Primes sum - GFG/primes-sum.java
@@ -31,7 +31,7 @@ class Solution {
     }
     
     public static boolean isPrime(int n) {
-        for(int i=2;i*i<=n;i++) {
+        for(int i=2;i*i<=n;i++) {   // for(int i=2; i<Math.sqrt(n); i++)
             if(n%i==0) 
                 return false;
         }
