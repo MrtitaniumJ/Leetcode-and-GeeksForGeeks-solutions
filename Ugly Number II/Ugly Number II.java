b41032742b9c164c02dce6e3f1ed8e528b4767ca
Jatin Sharma <87287391+MrtitaniumJ@users.noreply.github.com>
2022-11-18 19:39:39 +0530

    Create Ugly Number II.java

---
+++ Ugly Number II/Ugly Number II.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isUgly(int n) {
+        if(n == 0)
+            return false;
+        if(n == 1)
+            return true;
+ 
+        if(n%2 == 0){
+            n = n/2;
+            return isUgly(n);
+        }
+ 
+        if(n%3 == 0){
+            n = n/3;
+            return isUgly(n);
+        }
+ 
+        if(n%5==0){
+            n = n/5;
+            return isUgly(n);
+        }
+ 
+        return false;
+    }
+}
